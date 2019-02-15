67fe063a25955b5fc292f81cb3c5cf1322ca8d3f
Mark Pothecary <markpothers@hotmail.com>
2019-02-15 15:05:33 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -67,9 +67,10 @@ merged_array = array1
 
   merged_array.each do |merged_items|
     array2.each do |item|
+      item.each do |name, attributes|
+        if merged_items == 
       binding.pry
 
-          item.each do |name, attributes|
 
             end
     end
